5cbf72db7532abad4e0d20efdf603173b477ade1
Lukas Guziel <guziel@interaktiv.de>
2023-06-01 09:13:54 +0200

    ref #19877 add translations

--- src/AnimatedLayers/Schema.js
+++ src/AnimatedLayers/Schema.js
@@ -1,21 +1,146 @@
-const AnimationLayerSchema = () => ({
-  title: 'Animation Layer',
+import { defineMessages } from 'react-intl';
+
+const animationLayerSchemaMessages = defineMessages({
+  titleLabel: {
+    id: 'schema.animationLayer.title',
+    defaultMessage: 'Animation Layer',
+  },
+  fieldsetDefault: {
+    id: 'schema.animationLayer.fieldset.default',
+    defaultMessage: 'Default',
+  },
+  labelTitle: {
+    id: 'schema.animationLayer.label.title',
+    defaultMessage: 'Label',
+  },
+  labelDescription: {
+    id: 'schema.animationLayer.label.description',
+    defaultMessage: 'Label of the Checkbox',
+  },
+  animationTitle: {
+    id: 'schema.animationLayer.animation.title',
+    defaultMessage: 'Animation',
+  },
+  animationDescription: {
+    id: 'schema.animationLayer.animation.description',
+    defaultMessage: 'Select the file of the animated SVG.',
+  },
+  visibleTitle: {
+    id: 'schema.animationLayer.visible.title',
+    defaultMessage: 'Visible',
+  },
+  visibleDescription: {
+    id: 'schema.animationLayer.visible.description',
+    defaultMessage: 'Initially Visible?',
+  },
+  sizeTitle: {
+    id: 'schema.animationLayer.size.title',
+    defaultMessage: 'Scale Size',
+  },
+  sizeDescription: {
+    id: 'schema.animationLayer.size.description',
+    defaultMessage: 'Scale factor of the animated Layer. Range between 0 and 1.',
+  },
+  topTitle: {
+    id: 'schema.animationLayer.top.title',
+    defaultMessage: 'Position Top',
+  },
+  topDescription: {
+    id: 'schema.animationLayer.top.description',
+    defaultMessage: 'Css-Attribute "top". Value is in Percentage.',
+  },
+  leftTitle: {
+    id: 'schema.animationLayer.left.title',
+    defaultMessage: 'Position Left',
+  },
+  leftDescription: {
+    id: 'schema.animationLayer.left.description',
+    defaultMessage: 'Css-Attribute "left". Value is in Percentage.',
+  },
+});
+const callToActionButtonSchemaMessages = defineMessages({
+  buttonTitle: {
+    id: 'schema.callToActionButton.title',
+    defaultMessage: 'Button',
+  },
+  buttonFieldsetDefault: {
+    id: 'schema.callToActionButton.fieldset.default',
+    defaultMessage: 'Default',
+  },
+  buttonLabelTitle: {
+    id: 'schema.callToActionButton.label.title',
+    defaultMessage: 'Label',
+  },
+  buttonLabelDescription: {
+    id: 'schema.callToActionButton.label.description',
+    defaultMessage: 'Label of the Link',
+  },
+  buttonLinkTitle: {
+    id: 'schema.callToActionButton.link.title',
+    defaultMessage: 'Link',
+  },
+  buttonLinkDescription: {
+    id: 'schema.callToActionButton.link.description',
+    defaultMessage: 'External or Internal Link.',
+  },
+});
+const animatedLayersSchemaMessages = defineMessages({
+  defaultTitle: {
+    id: 'schema.animatedLayers.fieldset.default',
+    defaultMessage: 'Default',
+  },
+  titleLabel: {
+    id: 'schema.animatedLayers.title.title',
+    defaultMessage: 'Title',
+  },
+  titleDescription: {
+    id: 'schema.animatedLayers.title.description',
+    defaultMessage: 'Title of the Block (left side).',
+  },
+  descriptionLabel: {
+    id: 'schema.animatedLayers.description.title',
+    defaultMessage: 'Description',
+  },
+  descriptionDescription: {
+    id: 'schema.animatedLayers.description.description',
+    defaultMessage: 'Description of the Block (left side).',
+  },
+  layersTitle: {
+    id: 'schema.animatedLayers.layers.title',
+    defaultMessage: 'Animation Layers',
+  },
+  layersDescription: {
+    id: 'schema.animatedLayers.layers.description',
+    defaultMessage: 'Add Animation Layers.',
+  },
+  buttonsTitle: {
+    id: 'schema.animatedLayers.buttons.title',
+    defaultMessage: 'Call to Action Buttons',
+  },
+  buttonsDescription: {
+    id: 'schema.animatedLayers.buttons.description',
+    defaultMessage: 'Add a Call to Action Button.',
+  },
+});
+
+export const AnimationLayerSchema = ({ intl }) => ({
+  title: intl.formatMessage(animationLayerSchemaMessages.titleLabel),
   fieldsets: [
     {
       id: 'default',
-      title: 'Default',
+      title: intl.formatMessage(animationLayerSchemaMessages.fieldsetDefault),
       fields: ['label', 'animation', 'visible', 'size', 'top', 'left'],
     },
   ],
   properties: {
     label: {
-      title: 'Label',
-      description: 'Label of the Checkbox',
+      title: intl.formatMessage(animationLayerSchemaMessages.labelTitle),
+      description: intl.formatMessage(animationLayerSchemaMessages.labelDescription),
       default: '',
     },
     animation: {
-      title: 'Animation',
-      description: 'Select the file of the animated SVG.',
+      title: intl.formatMessage(animationLayerSchemaMessages.animationTitle),
+      description: intl.formatMessage(animationLayerSchemaMessages.animationDescription),
       widget: 'object_browser',
       widgetOptions: {
         pattern_options: { selectableTypes: ['File'] },
@@ -25,47 +150,47 @@ const AnimationLayerSchema = () => ({
       allowExternals: true,
     },
     visible: {
-      title: 'Visible',
-      description: 'Initially Visible?',
+      title: intl.formatMessage(animationLayerSchemaMessages.visibleTitle),
+      description: intl.formatMessage(animationLayerSchemaMessages.visibleDescription),
       type: 'boolean',
     },
     size: {
-      title: 'Scale Size',
-      description: 'Scale factor of the animated Layer. Range between 0 and 1.',
+      title: intl.formatMessage(animationLayerSchemaMessages.sizeTitle),
+      description: intl.formatMessage(animationLayerSchemaMessages.sizeDescription),
       default: '1.0',
     },
     top: {
-      title: 'Position Top',
-      description: 'Css-Attribute "top". Value is in Percentage.',
+      title: intl.formatMessage(animationLayerSchemaMessages.topTitle),
+      description: intl.formatMessage(animationLayerSchemaMessages.topDescription),
       default: '0',
     },
     left: {
-      title: 'Position Left',
-      description: 'Css-Attribute "left". Value is in Percentage.',
+      title: intl.formatMessage(animationLayerSchemaMessages.leftTitle),
+      description: intl.formatMessage(animationLayerSchemaMessages.leftDescription),
       default: '0',
     },
   },
   required: ['label', 'animation'],
 });
 
-const CallToActionButtonSchema = () => ({
-  title: 'Button',
+const CallToActionButtonSchema = ({ intl }) => ({
+  title: intl.formatMessage(callToActionButtonSchemaMessages.buttonTitle),
   fieldsets: [
     {
       id: 'default',
-      title: 'Default',
+      title: intl.formatMessage(callToActionButtonSchemaMessages.buttonFieldsetDefault),
       fields: ['label', 'link'],
     },
   ],
   properties: {
     label: {
-      title: 'Label',
-      description: 'Label of the Link',
+      title: intl.formatMessage(callToActionButtonSchemaMessages.buttonLabelTitle),
+      description: intl.formatMessage(callToActionButtonSchemaMessages.buttonLabelDescription),
       default: '',
     },
     link: {
-      title: 'Link',
-      description: 'External or Internal Link.',
+      title: intl.formatMessage(callToActionButtonSchemaMessages.buttonLinkTitle),
+      description: intl.formatMessage(callToActionButtonSchemaMessages.buttonLinkDescription),
       widget: 'object_browser',
       mode: 'single',
       return: 'single',
@@ -75,41 +200,39 @@ const CallToActionButtonSchema = () => ({
   required: ['label', 'link'],
 });
 
-export const AnimatedLayersSchema = (props) => {
-  return {
-    required: [],
-    fieldsets: [
-      {
-        id: 'default',
-        title: 'Default',
-        fields: ['title', 'description', 'layers', 'buttons'],
-      },
-    ],
-    properties: {
-      title: {
-        title: 'Title',
-        description: 'Title of the Block (left side).',
-        default: '',
-      },
-      description: {
-        title: 'Description',
-        description: 'Description of the Block (left side).',
-        default: '',
-      },
-      layers: {
-        title: 'Animation Layers',
-        description: 'Add Animation Layers.',
-        schema: AnimationLayerSchema({}),
-        widget: 'object_list',
-      },
-      buttons: {
-        title: 'Call to Action Buttons',
-        description: 'Add a Call to Action Button.',
-        schema: CallToActionButtonSchema({}),
-        widget: 'object_list',
-      },
+export const AnimatedLayersSchema = ({ intl }) => ({
+  required: [],
+  fieldsets: [
+    {
+      id: 'default',
+      title: intl.formatMessage(animatedLayersSchemaMessages.defaultTitle),
+      fields: ['title', 'description', 'layers', 'buttons'],
     },
-  };
-};
+  ],
+  properties: {
+    title: {
+      title: intl.formatMessage(animatedLayersSchemaMessages.titleLabel),
+      description: intl.formatMessage(animatedLayersSchemaMessages.titleDescription),
+      default: '',
+    },
+    description: {
+      title: intl.formatMessage(animatedLayersSchemaMessages.descriptionLabel),
+      description: intl.formatMessage(animatedLayersSchemaMessages.descriptionDescription),
+      default: '',
+    },
+    layers: {
+      title: intl.formatMessage(animatedLayersSchemaMessages.layersTitle),
+      description: intl.formatMessage(animatedLayersSchemaMessages.layersDescription),
+      schema: AnimationLayerSchema({ intl }),
+      widget: 'object_list',
+    },
+    buttons: {
+      title: intl.formatMessage(animatedLayersSchemaMessages.buttonsTitle),
+      description: intl.formatMessage(animatedLayersSchemaMessages.buttonsDescription),
+      schema: CallToActionButtonSchema({ intl }),
+      widget: 'object_list',
+    },
+  },
+});
 
 export default AnimatedLayersSchema;
